e24750e8bfa1e4dc2f89eaf3d75bb85afa6f88a3
Intrepid Panda <jerick999@gmail.com>
2018-08-29 11:09:08 -0600

    set up file structure, early tests, and early insert method

---
+++ test/trie-test.js
@@ -0,0 +1,77 @@
+import { assert } from 'chai';
+import Node from "../lib/node.js";
+import Trie from "../lib/trie.js";
+import fs from 'fs';
+
+describe('Trie', () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  it('should be an object', () => {
+    assert.isObject(trie);
+  });
+
+  it('should have a way of tracking of how many words are in it', () => {
+    assert.equal(trie.wordCount, 0);
+    assert.equal(trie.count(), 0);
+  });
+
+  it('should increment count when new words are added', () => {
+    assert.equal(trie.wordCount, 0);
+    
+    trie.insert('hello');
+    
+    assert.equal(trie.wordCount, 1);
+    
+    trie.insert('world');
+    
+    assert.equal(trie.wordCount, 2);
+    
+    trie.insert('hello');
+    trie.insert('world');
+    
+    assert.equal(trie.wordCount, 2);
+  });
+
+  it.skip('should be able to suggest words based on partial strings', () => {
+    trie.insert('hello');
+    trie.insert('world');
+
+    assert.equal(trie.suggest('he'), ['hello']);
+
+    trie.insert('hellen');
+
+    assert.equal(trie.suggest('he'), ['hello', 'hellen']);
+    assert.equal(trie.suggest('w'), ['world']);
+  });
+
+  it.skip('should be able to take in an array of words and insert them', () => {
+    const text = "/usr/share/dict/words";
+    const dictionary = fs.readFileSync(text).toString().trim().split('\n');
+
+    assert.equal(trie.wordCount, 0);
+
+    trie.populate(dictionary);
+
+    assert.equal(trie.wordCount, 235886);
+  });
+
+  it.skip('should be able to remove words the user no longer wants', () => {
+    const text = "/usr/share/dict/words";
+    const dictionary = fs.readFileSync(text).toString().trim().split('\n');
+
+    assert.equal(trie.wordCount, 0);
+
+    trie.populate(dictionary);
+
+    assert.equal(trie.wordCount, 235886);
+
+    trie.delete('hello');
+    trie.delete('world');
+
+    assert.equal(trie.wordCount, 235884);
+  });
+});
